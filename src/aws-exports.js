99b4f79ef0fa90885be7bdd9586dad3772e0de64
Asif Mohammad <asif.mohammad@winwire.com>
2024-07-04 00:00:49 +0530

    GraphQL API and schema changes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -23,7 +23,12 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ]
+    ],
+    "aws_appsync_graphqlEndpoint": "https://6lsp4cukcrbudjq57jcfdv4oz4.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-apa7aspkxvedlfwpnvgbmtxp5u"
 };
+
 
 export default awsmobile;
